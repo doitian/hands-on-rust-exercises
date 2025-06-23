7adb5c83899cd4f40b6334dceb71b01730d86357
ian <me@iany.me>
2025-06-23 23:13:20 +0800

    display final score in game over screen to provide player feedback

--- src/main.rs
+++ src/main.rs
@@ -154,6 +154,7 @@ impl State {
     fn end_game(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Game Over!");
+        ctx.print_centered(6, format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
         if ctx.key == Some(VirtualKeyCode::P) {
