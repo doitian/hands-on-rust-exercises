8aed8386bf1af13227c5cd8b65748cbdef19f1c1
ian <me@iany.me>
2025-07-01 20:09:57 +0800

    refactor: remove collision system and its integration

--- crates/dungeoncrawl/src/systems/mod.rs
+++ crates/dungeoncrawl/src/systems/mod.rs
@@ -1,7 +1,6 @@
 use bracket_lib::random::RandomNumberGenerator;
 use legion::Schedule;
 
-mod collisions;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -26,8 +25,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
@@ -40,8 +37,6 @@ pub fn build_monster_scheduler(rng: RandomNumberGenerator) -> Schedule {
         .add_system(random_move::random_move_system(rng))
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
